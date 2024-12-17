7107529c03a6fb1613d99d27196052f2a32fb22b
Fernando <cruzfernando.dev@gmail.com>
2024-12-17 15:04:54 -0300

    Add TextAreaInput component with autofocus and custom styling

---
+++ resources/js/Components/TextAreaInput.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const model = defineModel({
+    type: String,
+    required: true,
+});
+
+const textarea = ref(null);
+
+onMounted(() => {
+    if (textarea.value.hasAttribute('autofocus')) {
+        textarea.value.focus();
+    }
+});
+
+defineExpose({
+    focus: () => textarea.value.focus(),
+});
+</script>
+
+<template>
+    <textarea
+        class="form-control border rounded-0"
+        v-model="model"
+        ref="textarea"
+        rows="4" 
+        autocomplete="off"
+    ></textarea>
+</template>
+
+<style scoped>
+textarea:focus {
+    border-color: #343a40 !important;  
+    outline: none !important; 
+    box-shadow: none !important;  
+    resize: none;
+}
+</style>
