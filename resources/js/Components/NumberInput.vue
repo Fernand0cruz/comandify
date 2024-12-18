10fb801af6f95c088f5383d931eadea7cec10b4a
Fernando <cruzfernando.dev@gmail.com>
2024-12-18 11:52:38 -0300

    Add NumberInput component with autofocus and custom styling

---
+++ resources/js/Components/NumberInput.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const model = defineModel({
+    type: Number,
+    required: true,
+});
+
+const input = ref(null);
+
+onMounted(() => {
+    if (input.value.hasAttribute('autofocus')) {
+        input.value.focus();
+    }
+});
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+
+<template>
+    <input
+        type="number"
+        class="form-control border rounded-0"
+        v-model="model"
+        ref="input"
+        autocomplete="off"
+        min="0"
+    />
+</template>
+
+<style scoped>
+input:focus {
+    border-color: #343a40 !important;  
+    outline: none !important; 
+    box-shadow: none !important;  
+}
+</style>
