74a5516d285fe6f58e620fd5652cbeb724638b1d
Fernando <cruzfernando.dev@gmail.com>
2025-01-14 13:16:50 -0300

    Refactor AuthenticatedLayout component for improved button styling and organization

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,16 +1,24 @@
 <script setup>
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import { Link } from "@inertiajs/vue3";
-import { Menu, ArrowRightFromLine, X } from "lucide-vue-next";
+import {
+    Menu,
+    ArrowRightFromLine,
+    X,
+    ListOrdered,
+    PackageSearch,
+    Book,
+    Blinds,
+    BadgeHelp,
+    Settings,
+} from "lucide-vue-next";
 </script>
 
 <template>
     <div
         class="offcanvas offcanvas-start"
-        data-bs-scroll="true"
         tabindex="-1"
         id="offcanvasScrolling"
-        aria-labelledby="offcanvasScrolling"
     >
         <div class="offcanvas-header d-flex justify-content-between">
             <div>
@@ -21,36 +29,40 @@ import { Menu, ArrowRightFromLine, X } from "lucide-vue-next";
             </button>
         </div>
         <div class="offcanvas-body d-flex flex-column">
-            <ul class="list-unstyled mb-auto">
+            <ul class="list-unstyled">
                 <li>
                     <a
                         :href="route('order-slip.index')"
-                        class="inline-flex btn btn-dark rounded-0 items-center px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white w-100 mb-2"
+                        class="btn btn-dark rounded-0 px-4 py-2 text-white text-start w-100 mb-2"
                     >
+                        <ListOrdered class="me-2" />
                         Comandas
                     </a>
                 </li>
                 <li>
                     <a
                         :href="route('product.index')"
-                        class="inline-flex btn btn-dark rounded-0 items-center px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white w-100 mb-2"
+                        class="btn btn-dark rounded-0 px-4 py-2 text-white text-start w-100 mb-2"
                     >
+                        <PackageSearch class="me-2" />
                         Produtos
                     </a>
                 </li>
                 <li>
                     <a
                         :href="route('product.index')"
-                        class="inline-flex btn btn-dark rounded-0 items-center px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white w-100 mb-2"
+                        class="btn btn-dark rounded-0 px-4 py-2 text-white text-start w-100 mb-2"
                     >
+                        <Book class="me-2" />
                         Relatórios
                     </a>
                 </li>
                 <li>
                     <a
                         :href="route('product.index')"
-                        class="inline-flex btn btn-dark rounded-0 items-center px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white w-100 mb-2"
+                        class="btn btn-dark rounded-0 px-4 py-2 text-white text-start w-100 mb-2"
                     >
+                        <Blinds class="me-2" />
                         Notas
                     </a>
                 </li>
@@ -60,26 +72,28 @@ import { Menu, ArrowRightFromLine, X } from "lucide-vue-next";
                 <li>
                     <a
                         :href="route('product.index')"
-                        class="inline-flex btn btn-dark rounded-0 items-center px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white w-100 mb-2"
+                        class="btn btn-dark rounded-0 px-4 py-2 text-white text-start w-100 mb-2"
                     >
+                        <BadgeHelp class="me-2" />
                         Como usar
                     </a>
                 </li>
                 <li>
                     <a
                         :href="route('product.index')"
-                        class="inline-flex btn btn-dark rounded-0 items-center px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white w-100 mb-2"
+                        class="btn btn-dark rounded-0 px-4 py-2 text-white text-start w-100 mb-2"
                     >
+                        <Settings class="me-2" />
                         Configurações
                     </a>
                 </li>
             </ul>
         </div>
     </div>
-    
+
     <div>
         <header
-            class="bg-dark p-3 shadow d-flex justify-content-between align-items-center"
+            class="bg-dark p-3 d-flex justify-content-between align-items-center"
         >
             <div class="d-flex align-items-center gap-3">
                 <PrimaryButton
@@ -87,9 +101,9 @@ import { Menu, ArrowRightFromLine, X } from "lucide-vue-next";
                     type="button"
                     data-bs-toggle="offcanvas"
                     data-bs-target="#offcanvasScrolling"
-                    aria-controls="offcanvasScrolling"
                 >
-                    <Menu class="me-2" /> Menu
+                    <Menu class="me-2" />
+                    Menu
                 </PrimaryButton>
                 <div>
                     <span class="fw-bold fs-4 text-white">Comandify</span>
@@ -99,9 +113,10 @@ import { Menu, ArrowRightFromLine, X } from "lucide-vue-next";
                 :href="route('logout')"
                 as="button"
                 method="post"
-                class="inline-flex border btn btn-dark rounded-0 items-center px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white"
+                class="btn btn-dark border rounded-0 px-4 py-2 text-white"
             >
-                <ArrowRightFromLine class="me-2" /> Sair
+                <ArrowRightFromLine class="me-2" />
+                Sair
             </Link>
         </header>
 
