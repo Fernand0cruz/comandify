ad840a9085fc0f9eb34b382bc549497a6037507f
Fernando <cruzfernando.dev@gmail.com>
2025-01-30 20:41:40 -0300

    Remove unused VueTheMask import from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,6 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import VueTheMask from 'vue-the-mask';
 import "vue3-toastify/dist/index.css";
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
@@ -22,7 +21,6 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .use(VueTheMask)
             .mount(el);
     },
     progress: {
